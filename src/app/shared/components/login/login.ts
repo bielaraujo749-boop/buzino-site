236828649ea0073852580be2abf6034b045c3e61
agth007 <arthur202115@gmail.com>
2026-09-16 01:42:59 -0300

    features: melhorias de UX e mudança no routerlink
    Alteração nas cores de alguns elementos e redirecionamento automático do login para a página Home

--- src/app/shared/components/login/login.ts
+++ src/app/shared/components/login/login.ts
@@ -1,11 +1,7 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { RecuperacaoSenha } from '../recuperacao-senha/recuperacao-senha';
 
-import {
-  FormBuilder,
-  ReactiveFormsModule,
-  Validators
-} from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
@@ -13,59 +9,48 @@ import { AuthService } from '../../../core/services/auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    RouterLink,
-     RecuperacaoSenha
-  ],
+  imports: [ReactiveFormsModule, RouterLink, RecuperacaoSenha],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
-
   private readonly fb = inject(FormBuilder);
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
 
-   erroLogin = '';
+  erroLogin = '';
 
   @Output() fechar = new EventEmitter<void>();
 
   loginForm = this.fb.nonNullable.group({
-    email: ['', [
-      Validators.required,
-      Validators.email
-    ]],
+    email: ['', [Validators.required, Validators.email]],
 
-    senha: ['', [
-      Validators.required
-    ]]
+    senha: ['', [Validators.required]],
   });
 
   logar(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
 
-  if (this.loginForm.invalid) {
-    this.loginForm.markAllAsTouched();
-    return;
+    const { email, senha } = this.loginForm.getRawValue();
+
+    const sucesso = this.authService.login(email, senha);
+
+    if (!sucesso) {
+      this.erroLogin = 'E-mail ou senha incorretos.';
+      return;
+    }
+
+    this.erroLogin = '';
+
+    this.fecharLogin();
+
+    this.router.navigate(['/']);
   }
 
-  const { email, senha } = this.loginForm.getRawValue();
-
-  const sucesso = this.authService.login(email, senha);
-
-  if (!sucesso) {
-     this.erroLogin = 'E-mail ou senha incorretos.';
-    return;
-  }
-
-   this.erroLogin = '';
-
-  this.fecharLogin();
-
-  this.router.navigate(['/dashboard']);
-}
-
- fecharLogin(): void {
+  fecharLogin(): void {
     this.fechar.emit();
   }
 
@@ -77,18 +62,15 @@ export class Login {
     return this.loginForm.controls.senha;
   }
 
-  
   recuperacaoAberta = false;
-
 
   abrirRecuperacao(): void {
     this.recuperacaoAberta = true;
   }
 
-
   fecharRecuperacao(): void {
     this.recuperacaoAberta = false;
-  } 
+  }
 
   /*logar(): void {
 
@@ -129,5 +111,4 @@ export class Login {
     this.recuperacaoAberta = false;
   } 
     */
-
 }
